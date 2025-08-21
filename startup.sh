7c32cdd81afc83a8da5e5504468e9b706ef49558
Ariel Raizman <ariel.raizman@hotmail.com>
2025-08-20 20:37:27 -0500

    simple create admin script

--- startup.sh
+++ startup.sh
@@ -27,6 +27,35 @@ python manage.py migrate
 
 
 
+# Create admin user from environment variables
+
+
+echo "Checking for admin credentials..."
+
+
+if [ -n "$DJANGO_ADMIN_USERNAME" ] && [ -n "$DJANGO_ADMIN_PASSWORD" ]; then
+    echo "Creating/updating admin user..."
+    python -c "
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'research_data_app.settings')
+import django
+django.setup()
+from django.contrib.auth.models import User
+username = os.environ.get('DJANGO_ADMIN_USERNAME')
+password = os.environ.get('DJANGO_ADMIN_PASSWORD')
+email = os.environ.get('DJANGO_ADMIN_EMAIL', 'admin@example.com')
+if User.objects.filter(username=username).exists():
+    user = User.objects.get(username=username)
+    user.set_password(password)
+    user.email = email
+    user.save()
+    print(f'Updated existing admin user: {username}')
+else:
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f'Created new admin user: {username}')
+"
+fi
+
 
 
 # Start Gunicorn server
