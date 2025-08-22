98c7949388afa7dc8be6adb6700f0518b267ec7b
Ariel Raizman <ariel.raizman@hotmail.com>
2025-08-22 09:17:20 -0500

    removed smth oops

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export AZURE_BLOB_SAS_TOKEN="sv=2024-11-04&ss=bfqt&srt=sco&sp=rtfx&se=2025-08-22T22:04:17Z&spr=https&sig=x0Y3GoSQx1yv3cnJsn8Z1Xrsc2UBP7k3JwsjKlqu%2FKg%3D"
 # Start PostgreSQL service (if available)
 if command -v systemctl &> /dev/null; then
     sudo systemctl start postgresql
